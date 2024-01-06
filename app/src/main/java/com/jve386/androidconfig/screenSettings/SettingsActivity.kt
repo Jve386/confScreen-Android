09bc853505866708d10f800197fe514455f6dbdc
Jve386 <jve.formacio@gmail.com>
2024-01-06 11:06:50 +0100

    Switch working.

--- app/src/main/java/com/jve386/androidconfig/screenSettings/SettingsActivity.kt
+++ app/src/main/java/com/jve386/androidconfig/screenSettings/SettingsActivity.kt
@@ -1,31 +1,24 @@
 package com.jve386.androidconfig.screenSettings
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.jve386.androidconfig.R
-import kotlin.math.log
 
 class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val swtch = findViewById<Switch>(R.id.sbtnDarkmode) as SwitchMaterial
+        val swt = findViewById<SwitchMaterial>(R.id.swtDarkMode)
 
-        swtch.setOnClickListener{
-            if(swtch.isChecked){
+        swt.setOnClickListener {
+            if (swt.isChecked) {
                 Toast.makeText(applicationContext, "Switch is on", Toast.LENGTH_LONG).show()
-            }
-            else {
+            } else {
                 Toast.makeText(applicationContext, "Switch is off", Toast.LENGTH_LONG).show()
             }
-
         }
     }
 }
